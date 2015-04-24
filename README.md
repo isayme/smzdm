8da871f512e1c73c5da405fa3722971c2452d802
isayme <isaymeorg@gmail.com>
2015-04-24 21:48:11 +0800

    upload working version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# smzdm
-什么值得买每日签到奖励
+# 什么值得买 每日签到奖励 #
+此脚本用于 [什么值得买](http://www.smzdm.com/) 网站的每日签到.
+
+# 如何使用? #
+脚本语言是`Python`, 所以首先你要[安装Python](https://www.python.org/downloads/)
+
+然后脚本依赖于`requests`库, 所以你要[安装requests](http://www.python-requests.org/en/latest/user/install/)
+
+都安装完成之后, 还需要修改脚本中的`SMZDM_USERNAME`(你的账号名)及`SMZDM_PASSWORD`(你的密码)变量
+
+最后, 只需要添加`cron`定时任务即可!
+
+# 关于脚本 #
+2015年(具体什么时候我也不知道), 值得买引入防爬虫机制, 第一次访问页面会返回`521`错误, 页面内容为`JavaScript`脚本, 此脚本会设置一个名为`__jsl_clearance`的`Cookie`, 并刷新页面.
+
+此机制直接导致我[之前的签到脚本](https://gist.github.com/isayme/5efc1bf611da29a3121c)无效, 所以才有了今天的版本.
+
+# 联系我 #
+`email`: isaymeorg # gmail.com
---
+++ smzdm_daily.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+import requests
+import re
+
+SMZDM_USERNAME = '' # username or email
+SMZDM_PASSWORD = '' # password
+
+class SMZDMDailyException(Exception):
+    def __init__(self, req):
+        self.req = req
+    
+    def __str__(self):
+        return str(req)
+    
+class SMZDMDaily(object):
+    LOGIN_URL = 'http://www.smzdm.com/user/login/jsonp_check'
+    CHECKIN_URL = 'http://www.smzdm.com/user/qiandao/jsonp_checkin'
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.session = requests.Session()
+        
+    def checkin(self):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:20.0) Gecko/20100101 Firefox/20.0',
+            'Host': 'www.smzdm.com',
+        }
+        
+        params = {
+            'user_login': self.username,
+            'user_pass': self.password,
+        }
+        
+        r = self.session.get(self.LOGIN_URL, headers=headers)
+        
+        # 处理值得买防爬虫机制
+        if r.status_code == 521:
+            # find cookie
+            m = re.findall(r'push\("([^"]+)"\)', r.text)
+            cookies = dict(__jsl_clearance=''.join(m))
+            r = self.session.get(self.LOGIN_URL, params=params, cookies=cookies, headers=headers)
+
+        if r.status_code != 200:
+            raise SMZDMDailyException(r)
+        
+        r = self.session.get(self.CHECKIN_URL, headers=headers, cookies=cookies)
+        if r.status_code != 200:
+            raise SMZDMDailyException(r)
+            
+        data = r.text[1:-1]
+        jdata = json.loads(data)
+        
+        return jdata
+        
+if __name__ == '__main__':
+    try:
+        smzdm = SMZDMDaily(SMZDM_USERNAME, SMZDM_PASSWORD)
+        result = smzdm.checkin()
+    except SMZDMDailyException as e:
+        print e
+    except Exception as e:
+        print e
+    else:
+        print result
+        
